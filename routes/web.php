1ffd0fb7ba6139c852ef2bb510652c043e80980b
OctavianCahyadi <mr.octavianz@gmail.com>
2020-04-26 12:22:33 +0700

    second commit

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/admin', function () {
+    return view('admin');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
